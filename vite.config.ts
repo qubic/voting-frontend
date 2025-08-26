620fef1cfa5aeb4333242bdf9ad70d11b9c1483a
alexmf91 <alexmunoz91@gmail.com>
2025-08-26 17:08:29 +0900

    fix: remove VITE_ENABLE_PROXY config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { defineConfig, loadEnv, type UserConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
 import tailwindcss from '@tailwindcss/vite'
@@ -22,9 +22,7 @@ const defaultConfig: UserConfig = {
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-	const env = loadEnv(mode, process.cwd())
-
-	if (command === 'serve' && (mode === 'development' || env.VITE_ENABLE_PROXY === 'true')) {
+	if (command === 'serve' && mode === 'development') {
 		return {
 			...defaultConfig,
 			server: {
